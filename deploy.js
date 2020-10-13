42333dbfb43869435a9167a136b6b43e7b584b7b
Joshua Fontany <joshua.fontany@gmail.com>
2020-10-12 18:53:52 -0700

    10/2020

--- deploy.js
+++ deploy.js
@@ -6,8 +6,14 @@ const path = require('path');
 var sourceDir = path.resolve("plugins");
 var destinationDir = process.env.DEPLOYDIR;
 
-//delete the old plugins?
-
+//delete the old plugins
+var plugins = ["joshuafontany/svgtools", "joshuafontany/fontawesome5", "joshuafontany/gameicons"]
+plugins.forEach(p => {
+    if(fs.existsSync(path.resolve(destinationDir, p))){
+        fs.rmdirSync(path.resolve(destinationDir, p), { recursive: true });
+    } 
+});
+//debugger;
 // copy folder content
 fs_Extra.copy(sourceDir, destinationDir, function (error) {
     if (error) {
